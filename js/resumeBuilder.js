d0331d7b3687ba475c3dc79b8c0093593886d80e
David Souza <david.souza@pollux.com.br>
2017-11-30 08:34:30 -0200

    Server-Side Routes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,61 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+bio = {
+    "name": "David Souza",
+    "role": "Developer",
+    "contacts": {
+        "mobile": "+55 47 900000000",
+        "email": "david@gmail.com"
+    },
+    "welcomeMessage": "lorem ipsum welcomeMessage",
+    "skills": [
+        "java", "python", "javascript"
+    ],
+    "bioPic": "images/david.jpg"
+}
+
+work = {
+    "jobs": [
+        {
+            "title": "System Analyst",
+            "employer": "Pollux Automation",
+            "dates": "September 2015 - present",
+            "city": "Joinville",
+            "description": "Internet of things (IoT) <br>Industry 4.0 <br>Software development for the pharmaceutical market in track and trace <br>Automated assembly line system "
+        },
+        {
+            "title": "System Analyst",
+            "employer": "TWC",
+            "dates": "January 2015 - September 2015",
+            "city": "Budapest",
+            "description": "Smart Tv Development <br>Web development"
+        }
+    ]
+}
+
+education = {
+    "universities": [
+        {
+            "name": "University of São Paulo",
+            "country": "Brazil",
+            "course": "Computer Science",
+            "start": "2010",
+            "end": "2016"
+        },
+        {
+            "name": "Eötvös Loránd University",
+            "country": "Hungary",
+            "course": "Computer Science",
+            "start": "2014",
+            "end": "2015"
+        }
+    ]
+}
+
+projects = {
+    "publications": [
+        {
+            "title": "Interface to retrive info",
+            "dates": "17/01/2015",
+            "description": "Interface to retrive info"
+        }
+    ]
+}
